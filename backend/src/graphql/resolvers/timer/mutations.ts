eda4a90f0f909c9cc1ef90774eba3b51bb6d8cf8
Ewan Breakey <ebreakey@gmail.com>
2022-04-10 19:24:00 +1000

    Fix broken timer resolvers

--- backend/src/graphql/resolvers/timer/mutations.ts
+++ backend/src/graphql/resolvers/timer/mutations.ts
@@ -1,16 +1,24 @@
 import client from 'client'
 
 const startNewTimer = async (_parent, { input: data }) =>
-  client.timer.create({ data })
+  client.timer
+    .create({ data })
+    .then(timer => ({ timer }))
 
 const stopTimer = async (_parent, { input: { id, ...data } }) =>
-  client.timer.update({ where: { id }, data })
+  client.timer
+    .update({ where: { id }, data })
+    .then(timer => ({ timer }))
 
 const updateTimer = async (_parent, { input: { id, ...data }}) =>
-  client.timer.update({ where: { id }, data})
+  client.timer
+    .update({ where: { id }, data})
+    .then(timer => ({ timer }))
 
 const deleteTimer = async (_parent, { input: { id }}) =>
-  client.timer.delete({ where: { id }})
+  client.timer
+    .delete({ where: { id }})
+    .then(timer => ({ timer }))
 
 const timerMutatations = {
   startNewTimer,
