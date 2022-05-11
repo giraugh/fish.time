d259e851e6b95ff472c53401fca665f6caa55569
Ewan Breakey <ebreakey@gmail.com>
2022-05-11 23:13:03 +1000

    Setup gcloud resources

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,5 +1,14 @@
+import path from 'path'
 import { config } from 'dotenv'
+
+export const env = process.env.NODE_ENV || 'development'
+
+// Load env
 config()
+if (env === 'production') {
+  config({ path: path.resolve(process.cwd(), '.env.production'), override: true })
+  config({ path: path.resolve(process.cwd(), 'prod.env'), override: true })
+}
 
 export const PORT = process.env.PORT || 6001
 export const LAMBDA_KEY = process.env.LAMBDA_KEY as string
